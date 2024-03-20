559387d3b67fe85078a4a1249545b73532a2ec2b
Florian Llimos <105484041+LlimosF@users.noreply.github.com>
2024-03-20 21:26:29 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <link rel="icon" href="media/home/florian.png" />
+  <title>Florian Llimos</title>
+</head>
+<body>
+  
+  <?php 
+
+    require_once("component/loading.php");
+    require_once("home.php");
+    require_once("background.php");
+    require_once("component/header.php");
+
+  ?>
+
+</body>
+<script src="theme.js"></script>
+<script src="lang.js"></script>
+<script src="typing.js"></script>
+<script src="download.js"></script>
+</html>
