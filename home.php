c5beed98ac4a1f44ee5f634d3786b88f2d2613a5
Florian Llimos <105484041+LlimosF@users.noreply.github.com>
2024-03-20 21:25:31 +0100

    Create home.php

---
+++ home.php
@@ -0,0 +1,56 @@
+<div id="home">
+  <div class="container-typing">
+    <p id="typing">
+      Développeur 
+      <span class="typed-text"></span><span class="cursor">&nbsp;</span>
+    </p>
+  </div>
+  <div class="container-home-hello">
+    <p id="home-hello-text">Bonjour & Bienvenue</p>
+  </div>
+  <div class="container-home-picture">
+    <img src="media/home/florian.png" alt="Photo de moi même" id="home-picture">
+  </div>
+  <div class="container-home-download">
+    <button id="download" class="download-dark" title="Télécharger mon cv">Télécharger CV</button>
+  </div>
+  <div class="container-home-social-link" role="navigation">
+    <ul role="menu" class="ul-social-link">
+      <li role="menuitem">
+        <a href="https://www.instagram.com/f.lmss/?hl=fr" aria-label="Lien vers mon profil instagram" title="Lien vers mon profil Instagram">
+          <img src="media/theme/dark/instagram.png" id="instagram">
+        </a>
+      </li>  
+      <li role="menuitem">
+        <a href="https://www.facebook.com/florian.llimos/" aria-label="Lien vers mon profil Facebook" title="Lien vers mon profil Facebook">
+          <img src="media/theme/dark/facebook.png" id="facebook">
+        </a>
+      </li>
+      <li role="menuitem">
+        <a href="https://fr.linkedin.com/in/florian-llimos-a27944231" aria-label="Lien vers mon profil Linkedin" title="Lien vers mon profil Linkedin">
+          <img src="media/theme/dark/linkedin.png" id="linkedin">
+        </a>
+      </li>
+      <li role="menuitem">
+        <a href="https://github.com/LlimosF" aria-label="Lien vers mon profil Github" title="Lien vers mon profil Github">
+          <img src="media/theme/dark/github.png" id="github">
+        </a>
+      </li>
+      <li role="menuitem">
+        <a href="tel:0622005584" aria-label="Lien pour me téléphoner" title="Lien pour me téléphoner">
+          <img src="media/theme/dark/telephone.png" id="telephone">
+        </a>
+      </li>
+      <li role="menuitem">
+        <a href="mailto:llimosflorian1@gmail.com" aria-label="Lien pour m'envoyer un e-mail" title="Lien pour m'envoyer un e-mail">
+          <img src="media/theme/dark/mail.png" id="mail">
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div class="container-home-bottom">
+    <a href="#a-propos" aria-label="Aller vers la section à propos" title="Aller vers la section à propos">
+      <img src="media/theme/dark/arrow.png" alt="" id="home-bottom">
+    </a>
+  </div>
+</div>
