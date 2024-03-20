ff4684b61364419b14b900b4102a990995c91000
Florian Llimos <105484041+LlimosF@users.noreply.github.com>
2024-03-20 21:26:06 +0100

    Create loading.php

---
+++ component/loading.php
@@ -0,0 +1,7 @@
+<div id="container-loading">
+  <div class="container-loading-text">
+    <p>
+      <span class="container-loading-text-first">&lt;</span><span class="container-loading-text-second">/</span><span class="container-loading-text-last">&gt;</span>
+    </p>
+  </div>
+</div>
