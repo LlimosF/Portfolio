5ab4781f73fb646bc483f27c663a0f6ffe8b3b1b
LlimosF <llimosflorian1>
2024-03-29 07:52:48 +0100

    Création du composant portfolio

--- skills.php
+++ skills.php
@@ -1,5 +1,5 @@
 <div id="skills">
-  <h2 class="title-section" id="title-skills">Languages</h2>
+  <h2 class="title-section" id="title-skills">Compétences</h2>
   <div class="container-skills">
     <div class="container-skills-bloc section container-skills-html">
       <h3 class="title-card">html</h3>
@@ -34,7 +34,7 @@
       <img src="media/skills/jquery.png" class="img-skill" />
     </div>
     <div class="container-skills-bloc section container-skills-html">
-      <h3 class="title-card">Joomlas</h3>
+      <h3 class="title-card">Joomla</h3>
       <img src="media/skills/joomla.png" class="img-skill" />
     </div>
     <div class="container-skills-bloc section container-skills-html">
@@ -48,57 +48,6 @@
     <div class="container-skills-bloc section container-skills-html">
       <h3 class="title-card">React</h3>
       <img src="media/skills/react.png" class="img-skill" />
-    </div>
-  </div>
-  <div class="container-skills">
-    <div class="container-skills-card">
-      <div class="skills-card">
-        <h3 class="container-skills-card-title" id="skills-title-rgaa">Accessibilité</h3>
-        <div class="container-skills-card-stars">
-          <img src="media/skills/star-pleine.png">
-          <img src="media/skills/star-pleine.png">
-          <img src="media/skills/star-pleine.png">
-          <img src="media/skills/star-vide.png">
-          <img src="media/skills/star-vide.png">
-        </div>
-        <img src="media/skills/accessibilite.png" class="container-skills-card-logo">
-      </div>
-    </div>
-    <hr class="separator">
-    <div class="skills-card">
-      <h3 class="container-skills-card-title" id="skills-title-responsive">Adaptabilité</h3>
-      <div class="container-skills-card-stars">
-        <img src="media/skills/star-pleine.png">
-        <img src="media/skills/star-pleine.png">
-        <img src="media/skills/star-pleine.png">
-        <img src="media/skills/star-pleine.png">
-        <img src="media/skills/star-pleine.png">
-      </div>
-      <img src="media/skills/responsive.png" class="container-skills-card-logo">
-    </div>
-    <hr class="separator">
-    <div class="skills-card">
-      <h3 class="container-skills-card-title" id="skills-title-seo">Réferencement</h3>
-      <div class="container-skills-card-stars">
-        <img src="media/skills/star-pleine.png">
-        <img src="media/skills/star-pleine.png">
-        <img src="media/skills/star-pleine.png">
-        <img src="media/skills/star-vide.png">
-        <img src="media/skills/star-vide.png">
-      </div>
-      <img src="media/skills/seo.png" class="container-skills-card-logo">
-    </div>
-    <hr class="separator">
-    <div class="skills-card">
-      <h3 class="container-skills-card-title" id="skills-title-seo">Sécurité</h3>
-      <div class="container-skills-card-stars">
-        <img src="media/skills/star-pleine.png">
-        <img src="media/skills/star-pleine.png">
-        <img src="media/skills/star-pleine.png">
-        <img src="media/skills/star-pleine.png">
-        <img src="media/skills/star-vide.png">
-      </div>
-      <img src="media/skills/shield.png" class="container-skills-card-logo">
     </div>
   </div>
 </div>
