9dd03e565d252e139fabdfc376129721505ba737
Florian Llimos <105484041+LlimosF@users.noreply.github.com>
2024-04-02 13:41:13 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,6 +4,40 @@
     <form class="form-newsletter">
       <input type="email" required placeholder="Adresse e-mail">
       <button type="submit">Inscription</button>
+      <div class="bloc-newsletter">
+        
+        <?php
+    
+        require_once("component/database.php");
+
+        if(!empty($_POST)) {
+
+          $email = htmlspecialchars($_POST["email"];
+
+          if(isset($_POST["email"]) && !empty($_POST["email"])) {
+        
+            $sql = "INSERT INTO `newsletter`(email) VALUES (:email)";
+              
+            $query = $db->prepare($sql);
+            $query->bindValue(":email", $email, PDO::PARAM_STR);
+            $query->execute();
+        
+            if($query->execute()){
+        
+              echo "<span class='success'>Vous êtes bien inscrit à la newsletter.</span>";
+        
+            } else {
+        
+              echo "<span class='error'>Erreur lors de l'inscription à la newsletter.</span>";
+        
+            }
+        
+          }
+
+        }
+
+      ?>
+      </div>  
     </form>
   </div>
   <p class="copyright">Copyright 2024 Florian LLIMOS.</p>
