15586d858d5faa5404ef3afce979b31b4cb27c35
Florian Llimos <105484041+LlimosF@users.noreply.github.com>
2024-04-03 21:06:09 +0200

    Update about.php

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
     <div class="container-about-bloc-number">
         <span class="container-about-bloc-number-span">1</span>
       </div>
-      <img src="media/about/selfi.png" alt="" class="container-about-logo">
+      <img src="media/about/selfi.png" alt="Icone développeur web" class="container-about-logo">
       <p class="color-dark" id="about-presentation">
         Je m'appelle Florian, j'ai 24 ans, et je suis un développeur web passionné basé près de Toulouse. Mon voyage dans le monde du développement web a commencé il y a quelques années, 
         et depuis lors, j'ai travaillé dur pour acquérir des compétences techniques solides et une profonde compréhension des technologies web modernes.
@@ -15,7 +15,7 @@
       <div class="container-about-bloc-number">
         <span class="container-about-bloc-number-span">2</span>
       </div>
-      <img src="media/about/diplome.png" alt="" class="container-about-logo">
+      <img src="media/about/diplome.png" alt="Icone de diplôme" class="container-about-logo">
       <p class="color-dark" id="about-diplome">
         Après avoir découvert ma passion pour la programmation web, j'ai entrepris une formation en développement web qui m'a permis de maîtriser les langages et les outils essentiels 
         tels que HTML, CSS, JavaScript, et PHP. J'ai également acquis de l'expérience dans la création de sites web réactifs et conviviaux, en utilisant des frameworks tels que React et Angular.
@@ -25,7 +25,7 @@
       <div class="container-about-bloc-number">
         <span class="container-about-bloc-number-span">3</span>
       </div>
-      <img src="media/about/passion.png" alt="" class="container-about-logo">
+      <img src="media/about/passion.png" alt="Icone de passion" class="container-about-logo">
       <p class="color-dark" id="about-passion">
         Ce qui me motive le plus dans le développement web, c'est la possibilité de créer des expériences en ligne exceptionnelles qui répondent aux besoins des utilisateurs. 
         J'aime résoudre des problèmes complexes, concevoir des interfaces utilisateur intuitives et rendre le web plus accessible pour tous.
@@ -35,7 +35,7 @@
     <div class="container-about-bloc-number">
         <span class="container-about-bloc-number-span">4</span>
       </div>
-      <img src="media/about/passion.png" alt="" class="container-about-logo">
+      <img src="media/about/passion.png" alt="Icone d'expérience" class="container-about-logo">
       <p class="color-dark" id="about-recherche">
         Au fil des ans, j'ai eu l'opportunité de travailler sur divers projets passionnants, allant de sites web d'entreprises à des applications web interactives. 
         Chacun de ces projets a été l'occasion d'apprendre et de grandir en tant que développeur. Je suis fier de partager certains de ces projets dans mon portfolio.
@@ -45,7 +45,7 @@
     <div class="container-about-bloc-number">
         <span class="container-about-bloc-number-span">5</span>
       </div>
-      <img src="media/about/idee.png" alt="" class="container-about-logo">
+      <img src="media/about/idee.png" alt="Icone d'avenir" class="container-about-logo">
       <p class="color-dark" id="about-idee">
         Actuellement, je suis ouvert à de nouvelles opportunités professionnelles dans le domaine du développement web. Je suis enthousiaste à l'idée de contribuer à 
         des projets innovants, de collaborer avec d'autres passionnés de technologie, et de continuer à développer mes compétences.
